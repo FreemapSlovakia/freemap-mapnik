f2b9e31a4ecdf2b39100a4a88d9ad3aae9ddbb68
Martin Ždila <m.zdila@gmail.com>
2018-10-27 22:10:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+gdal_contour -i 10 -a height N48E020.HGT N48E020_10m.shp
+shp2pgsql -a -g way -s 4326:900913 N48E020_10m.shp contour | psql
+shp2pgsql -p -I -g way -s 4326:900913 N48E020_10m.shp contour | psql
+
+gdaldem hillshade -s 111120 -compute_edges -multidirectional N48E020.HGT N48E020.tif
+gdal_translate -of GTiff -co "TILED=YES" -a_srs "+proj=latlong" N48E020.tif N48E020_adapted.tif
+gdalwarp -of GTiff -co "TILED=YES" -srcnodata 32767 -t_srs "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over" -rcs -order 3 -tr 30 30 -multi N48E020_adapted.tif N48E020_warped.tif
+
+
+~/go/bin/imposm import -connection postgis://martin:b0n0@localhost/martin  -mapping imposm-mapping.json -read slovakia-latest.osm.pbf -write
---
+++ index.js
@@ -0,0 +1,95 @@
+const mapnik = require('mapnik');
+const { promisify } = require('util');
+const Koa = require('koa');
+const Router = require('koa-router');
+const send = require('koa-send');
+const genericPool = require('generic-pool');
+const { exec } = require('child_process');
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/:zoom/:x/:y', async (ctx) => {
+  const { zoom, x, y } = ctx.params;
+  await render(Number.parseInt(zoom), Number.parseInt(x), Number.parseInt(y));
+  await send(ctx, `tiles/tile_${zoom}_${x}_${y}.png`);
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(3000);
+
+mapnik.register_default_fonts();
+mapnik.register_default_input_plugins();
+
+// const writeFileAsync = promisify(fs.writeFile);
+mapnik.Map.prototype.loadAsync = promisify(mapnik.Map.prototype.load);
+mapnik.Map.prototype.fromStringAsync = promisify(mapnik.Map.prototype.fromString);
+mapnik.Map.prototype.renderAsync = promisify(mapnik.Map.prototype.render);
+mapnik.Map.prototype.renderFileAsync = promisify(mapnik.Map.prototype.renderFile);
+mapnik.Image.prototype.encodeAsync = promisify(mapnik.Image.prototype.encode);
+// const execAsync = promisify(exec);
+
+// +proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over
+// +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs
+
+const merc = new mapnik.Projection('+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over');
+
+const factory = {
+  async create() {
+    const map = new mapnik.Map(256, 256);
+    const xml = await new Promise((resolve, reject) => {
+      exec('xmllint --noent style/stylesheet.xml', (err, out) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(out);
+        }
+      });
+    });
+    await map.fromStringAsync(xml);
+    return map;
+  },
+  async destroy(map) {
+    // TODO ?
+  },
+};
+
+const opts = {
+  max: 8, // maximum size of the pool
+  min: 8 // minimum size of the pool
+};
+
+const pool = genericPool.createPool(factory, opts);
+
+function transformCoords(zoom, xtile, ytile) {
+  const n = Math.pow(2, zoom);
+  const lon_deg = xtile / n * 360.0 - 180.0
+  const lat_rad = Math.atan(Math.sinh(Math.PI * (1 - 2 * ytile / n)))
+  const lat_deg = lat_rad * 180.0 / Math.PI
+  return [lon_deg, lat_deg];
+}
+
+async function render(zoom, x, y) {
+  const map = await pool.acquire();
+  map.zoomToBox(merc.forward([...transformCoords(zoom, x, y + 1), ...transformCoords(zoom, x + 1, y)]));
+  map.renderFileAsync = promisify(map.renderFile);
+  await map.renderFileAsync(`tiles/tile_${zoom}_${x}_${y}.png`, { format: 'png' });
+  // console.log(`saved map image to tile_${zoom}_${x}_${y}.png`);
+
+  pool.release(map);
+
+  // map.zoomAll();
+  // map.zoomToBox(20, 48, 22, 49);
+  // console.log('XXX', merc.forward([20, 48, 22, 49]));
+  // map.zoomToBox(merc.forward([20, 48, 22, 49]));
+  // map.zoomToBox(merc.forward([...transformCoords(9, 286, 176), ...transformCoords(9, 287, 175)]));
+
+  // await map.renderFileAsync('map.pdf', { format: 'pdf' });
+  // const im = new mapnik.Image(256, 256);
+  // await map.renderAsync(im);
+  // const buffer = await im.encodeAsync('pdf');
+  // await writeFileAsync('map.pdf', buffer);
+}
