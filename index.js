b9e6d36705b0368b44312b2a08516548489ed77b
Martin Ždila <m.zdila@gmail.com>
2018-10-31 20:16:55 +0100

    implemented basic tile caching and configuration

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const { mkdir, rename, exists } = require('fs');
+const path = require('path');
 const mapnik = require('mapnik');
 const { promisify } = require('util');
+const config = require('config');
 const Koa = require('koa');
 const Router = require('koa-router');
 const send = require('koa-send');
@@ -10,17 +13,24 @@ const { mercSrs } = require('./lib/projections');
 const app = new Koa();
 const router = new Router();
 
+const existsAsync = promisify(exists);
+const mkdirAsync = promisify(mkdir);
+const renameAsync = promisify(rename);
+
+const tilesDir = config.get('tilesDir');
+const serverPort = config.get('server.port');
+
 router.get('/:zoom/:x/:y', async (ctx) => {
   const { zoom, x, y } = ctx.params;
   await render(Number.parseInt(zoom), Number.parseInt(x), Number.parseInt(y));
-  await send(ctx, `tiles/tile_${zoom}_${x}_${y}.png`);
+  await send(ctx, `${tilesDir}/${zoom}/${x}/${y}.png`);
 });
 
 app
   .use(router.routes())
   .use(router.allowedMethods());
 
-app.listen(3000);
+app.listen(serverPort);
 
 mapnik.register_default_fonts();
 mapnik.register_default_input_plugins();
@@ -47,8 +57,8 @@ const factory = {
 };
 
 const opts = {
-  max: 8, // maximum size of the pool
-  min: 8 // minimum size of the pool
+  max: 8, // maximum size of the pool (TODO make configurable)
+  min: 8, // minimum size of the pool (TODO make configurable)
 };
 
 const pool = genericPool.createPool(factory, opts);
@@ -67,6 +77,33 @@ async function render(zoom, x, y) {
   const map = await pool.acquire();
   map.zoomToBox(merc.forward([...transformCoords(zoom, x, y + 1), ...transformCoords(zoom, x + 1, y)]));
   map.renderFileAsync = promisify(map.renderFile);
-  await map.renderFileAsync(`tiles/tile_${zoom}_${x}_${y}.png`, { format: 'png' });
+
+  const frags = [tilesDir, zoom.toString(10), x.toString(10)];
+
+  const p = path.join(...frags, `${y}`);
+  if (!await existsAsync(`${p}.png`)) {
+    await mkdirFull(frags);
+    await map.renderFileAsync(`${p}_tmp.png`, { format: 'png' });
+    await renameAsync(`${p}_tmp.png`, `${p}.png`);
+  }
+
   pool.release(map);
+}
+
+async function mkdirFull(frags) {
+  if (!frags.length) {
+    return;
+  }
+  const p = path.join(...frags);
+  if (!await existsAsync(p)) {
+    await mkdirFull(frags.slice(0, frags.length - 1));
+    try {
+      await mkdirAsync(p);
+    } catch (e) {
+      // parallel creations may cause error
+      if (!await existsAsync(p)) {
+        throw e;
+      }
+    }
+  }
 }
