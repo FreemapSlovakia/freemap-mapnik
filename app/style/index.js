2dd18f5a84487cba247b487f921c3b5346621e61
Martin Ždila <m.zdila@gmail.com>
2018-11-22 18:16:33 +0100

    fixed valley label

--- app/style/index.js
+++ app/style/index.js
@@ -178,8 +178,8 @@ module.exports = function generateFreemapStyle() {
       .rule({ minZoom: 15 })
         .textSymbolizer({ ...fontDflt, fill: '#3d1d1d', placement: 'line', spacing: 200 }, '[name]')
     .style('feature_line_names')
-      .rule({ filter: '[type] = valley', minZoom: 13, spacing: 200 })
-        .textSymbolizer({ ...fontDflt, size: 16, opacity: 0.5, haloOpacity: 0.5, placement: 'line'  }, '[name]') // TODO size by zoom as for placenames
+      .rule({ filter: '[type] = valley', minZoom: 13 })
+        .textSymbolizer({ ...fontDflt, size: 16, opacity: 0.5, haloOpacity: 0.5, placement: 'line', spacing: 400 }, '[name]') // TODO size by zoom as for placenames
     .style('water_line_names')
       .rule({ minZoom: 12, filter: "[type] = 'river'" })
         .textSymbolizer({ ...fontDflt, fill: 'blue', placement: 'line', spacing: 400 }, '[name]')
