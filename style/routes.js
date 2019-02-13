3f52351ec3b85cd79f5ef24fe9badb7e4c67b75f
Martin Ždila <m.zdila@gmail.com>
2019-02-13 20:32:40 +0100

    made routes thinner for lowzoom (refs #62)

--- style/routes.js
+++ style/routes.js
@@ -18,26 +18,27 @@ function routes(...types) {
     for (let zoomVar = 0; zoomVar < 2; zoomVar++) {
       const zoomParams = { minZoom: zoomVar === 0 ? 12 : 9, maxZoom: zoomVar === 0 ? undefined : 11 };
       const zo = zoomVar === 0 ? 3 : 1;
+      const wf = zoomVar === 0 ? 2 : 1.5;
 
       for (const color of routeColors) {
         if (isHiking) {
           // major hiking
           style.rule({ filter: `[h_${color}] > 0`, ...zoomParams }).lineSymbolizer({
             stroke: mapColor(color),
-            strokeWidth: 2,
+            strokeWidth: wf,
             strokeLinejoin: 'round',
             strokeLinecap: 'butt',
-            offset: `${zo} + ([h_${color}] - 1) * 2`,
+            offset: `${zo} + ([h_${color}] - 1) * ${wf}`,
           });
 
           // local hiking
           style.rule({ filter: `[h_${color}_loc] > 0`, ...zoomParams }).lineSymbolizer({
             stroke: mapColor(color),
-            strokeWidth: 2,
+            strokeWidth: wf,
             strokeLinejoin: 'round',
             strokeLinecap: 'butt',
-            offset: `${zo} + ([h_${color}_loc] - 1) * 2`,
-            strokeDasharray: '6,4',
+            offset: `${zo} + ([h_${color}_loc] - 1) * ${wf}`,
+            strokeDasharray: `${wf * 3},${wf * 2}`,
           });
         }
 
@@ -45,32 +46,32 @@ function routes(...types) {
           // bicycle
           style.rule({ filter: `[b_${color}] > 0`, ...zoomParams }).lineSymbolizer({
             stroke: mapColor(color),
-            strokeWidth: 4,
+            strokeWidth: wf * 2,
             strokeLinejoin: 'round',
             strokeLinecap: 'round',
-            offset: `-(${zo} + ([b_${color}] - 1) * 4) - 1`,
-            strokeDasharray: '0.001,6',
+            offset: `-(${zo} + ([b_${color}] - 1) * ${wf * 2}) - 1`,
+            strokeDasharray: `0.001,${wf * 3}`,
           });
         }
 
         if (isSki) {
-          const offset = `-(${zo} + ([s_${color}] - 1) * 4) - 1`;
+          const offset = `-(${zo} + ([s_${color}] - 1) * ${wf * 2}) - 1`;
 
           style.rule({ filter: `[s_${color}] > 0`, ...zoomParams })
             .lineSymbolizer({
               stroke: 'orange',
-              strokeWidth: 4,
+              strokeWidth: wf * 2,
               strokeLinejoin: 'round',
               strokeLinecap: 'round',
               offset,
             })
             .lineSymbolizer({
               stroke: mapColor(color),
-              strokeWidth: 2,
+              strokeWidth: wf,
               strokeLinejoin: 'round',
               strokeLinecap: 'butt',
               offset,
-              strokeDasharray: '6,2',
+              strokeDasharray: `${wf * 3},${wf}`,
             });
         }
       }
