c887a0ee475db57463c70cb9a443cc867b073fa8
Martin Ždila <m.zdila@gmail.com>
2019-03-07 22:44:53 +0100

    improved relief composition

--- shading/gen.sh
+++ shading/gen.sh
@@ -1,39 +1,28 @@
 #!/bin/bash
 
-# intensity, color, az, out
 gen_relief() {
-  ./gen_ramp.js $2 $1 > shade.ramp \
-    && gdaldem hillshade hgt_warped.tiff shade.tiff -az $3 -igor -z 1.5 \
-    && gdaldem color-relief shade.tiff shade.ramp $4 -alpha \
-    && rm shade.ramp shade.tiff
+  gdaldem hillshade hgt_warped.tiff rel_$1.tiff -az $2 -igor -z 2
 }
 
-echo dark
+gen_relief a -120
+gen_relief b   60
+gen_relief c  -45
 
-gen_relief 0.8 2040B0 -120 out1.tiff
-gen_relief 0.6 FFEE00 60 out2.tiff
-gen_relief 0.8 000000 -45 out3.tiff
+a='0.8 * (255 - A)'
+b='1.0 * (255 - B)'
+c='1.0 * (255 - C)'
 
-echo blend
+gdal_calc.py -A rel_a.tiff -B rel_b.tiff -C rel_c.tiff --outfile=R.tiff \
+  --calc="(${a} * 0x20 + ${b} * 0xFF + ${c} * 0x00) / (0.01 + ${a} + ${b} + ${c})"
+gdal_calc.py -A rel_a.tiff -B rel_b.tiff -C rel_c.tiff --outfile=G.tiff \
+  --calc="(${a} * 0x40 + ${b} * 0xEE + ${c} * 0x00) / (0.01 + ${a} + ${b} + ${c})"
+gdal_calc.py -A rel_a.tiff -B rel_b.tiff -C rel_c.tiff --outfile=B.tiff \
+  --calc="(${a} * 0xB0 + ${b} * 0x00 + ${c} * 0x00) / (0.01 + ${a} + ${b} + ${c})"
+gdal_calc.py -A rel_a.tiff -B rel_b.tiff -C rel_c.tiff --outfile=A.tiff \
+  --calc="255.0 - 255.0 * ((1.0 - ${a} / 255.0) * (1.0 - ${b} / 255.0) * (1.0 - ${c} / 255.0))"
 
-./blend.sh out2.tiff out1.tiff tmp.tiff
-./blend.sh out3.tiff tmp.tiff final.tiff
+rm final.tiff rel_a.tiff rel_b.tiff rel_c.tiff
 
-echo light
+gdal_merge.py -separate -o final.tiff R.tiff G.tiff B.tiff A.tiff
 
-gen_relief 0.4 2040B0 -120 out1.tiff
-gen_relief 0.3 FFEE00 60 out2.tiff
-gen_relief 0.5 000000 -45 out3.tiff
-
-echo blend
-
-./blend.sh out2.tiff out1.tiff tmp.tiff
-./blend.sh out3.tiff tmp.tiff final_a.tiff
-
-#gdaldem slope hgt_warped.tiff shade.tiff
-#gdaldem color-relief shade.tiff color_slope.ramp out4.tiff
-
-# #######
-# gdal_translate -of GTiff -co "TILED=YES" -a_ullr 21 48 20 49 -a_srs "+proj=latlong" merged.png output.tif
-# gdalwarp -of GTiff -co "TILED=YES" -srcnodata 32767 -t_srs "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over" -rcs -order 3 -tr 30 30 -multi output.tif o2.tif
-
+rm R.tiff G.tiff B.tiff A.tiff
