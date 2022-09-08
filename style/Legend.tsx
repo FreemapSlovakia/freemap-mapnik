a3fcf7c1e04fada5d3b5fe5df286fa68be25a96e
Martin Ždila <m.zdila@gmail.com>
2022-09-08 18:59:28 +0200

    fixed legend

--- style/Legend.tsx
+++ style/Legend.tsx
@@ -1,9 +1,9 @@
-import { Datasource, Layer } from "jsxnik/mapnikConfig";
+import { Layer, StyleName } from "jsxnik/mapnikConfig";
 import { DatasourceEx } from "./DatasourceEx";
 
 export type Props = {
   legendLayers: {
-    styles: string;
+    styles: string[];
     geojson: unknown; // TODO GeoJSON
   }[];
 };
@@ -12,7 +12,11 @@ export function Legend({ legendLayers }: Props) {
   return (
     <>
       {legendLayers.map((layer) => (
-        <Layer name={layer.styles} srs="+init=epsg:4326">
+        <Layer srs="+init=epsg:4326">
+          {layer.styles.map((style) => (
+            <StyleName>{style}</StyleName>
+          ))}
+
           <DatasourceEx
             params={{
               type: "geojson",
