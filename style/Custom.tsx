14b17bb752fe2e5bced3e53a41981dd1879924be
Martin Ždila <m.zdila@gmail.com>
2022-09-08 19:40:36 +0200

    fixed (hacked) custom (drawing) layer

--- style/Custom.tsx
+++ style/Custom.tsx
@@ -2,14 +2,14 @@ import { Layer, Raw, StyleName } from "jsxnik/mapnikConfig";
 import { DatasourceEx } from "./DatasourceEx";
 
 export type CustomProps = {
-  styles: string[];
+  styles: any[];
   layers: { styles: string[]; geojson: string }[];
 };
 
 export function Custom(custom: CustomProps) {
   return (
     <>
-      <Raw>{custom.styles}</Raw>
+      <Raw>{custom.styles[0].style /* it is hacky to use freemap-mapserver without modification */}</Raw>
 
       {custom.layers.map((layer) => (
         <Layer srs="+init=epsg:4326">
