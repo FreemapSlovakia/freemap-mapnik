32c5fd163e1d042531850b023e7b0273875c5453
Martin Ždila <m.zdila@gmail.com>
2022-09-25 11:13:08 +0200

    added fixme indication also to ways (fixes #254)

--- style/Fixmes.tsx
+++ style/Fixmes.tsx
@@ -6,11 +6,26 @@ export function Fixmes() {
     <>
       <Style name="fixmes">
         <Rule>
-          <MarkersSymbolizer file="images/fixme.svg" />
+          <MarkersSymbolizer file="images/fixme.svg" opacity={0.75} />
+        </Rule>
+      </Style>
+
+      <Style name="fixmes_lines">
+        <Rule>
+          <MarkersSymbolizer
+            file="images/fixme.svg"
+            spacing={100}
+            placement="line"
+            opacity={0.75}
+            ignorePlacement
+            direction="auto"
+          />
         </Rule>
       </Style>
 
       <SqlLayer styleName="fixmes" sql="SELECT geometry FROM osm_fixmes" minZoom={14} />
+
+      <SqlLayer styleName="fixmes_lines" sql="SELECT geometry FROM osm_fixmes_lines" minZoom={14} />
     </>
   );
 }
