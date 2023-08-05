57810cad44ee57a373f8b9b2d7a571f13b1aa7ff
Martin Ždila <m.zdila@gmail.com>
2023-08-05 10:26:45 +0200

    fixed duplicating some names (fixes #292)

--- style/LandcoverNames.tsx
+++ style/LandcoverNames.tsx
@@ -1,5 +1,5 @@
-import { Style, TextSymbolizer } from "jsxnik/mapnikConfig";
-import { colors, hsl } from "./colors";
+import { Style } from "jsxnik/mapnikConfig";
+import { colors } from "./colors";
 import { TextSymbolizerEx } from "./TextSymbolizerEx";
 import { Placements } from "./Placements";
 import { RuleEx } from "./RuleEx";
@@ -33,27 +33,32 @@ export function LandcoverNames() {
         })}
       </Style>
 
+      {/* nested sql is to remove duplicate entries imported by imposm because we use `mappings` in yaml */}
       <SqlLayer
         styleName="landcover_names"
         minZoom={12}
         bufferSize={1024}
         sql="
-          SELECT
-            osm_landusages.geometry, osm_landusages.name, osm_landusages.area,
-            osm_landusages.type IN ('forest', 'wood', 'scrub', 'heath', 'grassland', 'scree', 'meadow', 'fell', 'wetland') AS natural
-          FROM
-            osm_landusages
-          LEFT JOIN
-            z_order_landuse USING (type)
-          LEFT JOIN
-            osm_feature_polys USING (osm_id)
-          LEFT JOIN
-            -- NOTE filtering some POIs (hacky because it affects also lower zooms)
-            osm_sports on osm_landusages.osm_id = osm_sports.osm_id AND osm_sports.type IN ('soccer', 'tennis', 'basketball')
-          WHERE
-            osm_feature_polys.osm_id IS NULL AND osm_sports.osm_id IS NULL AND osm_landusages.geometry && !bbox!
-          ORDER BY
-            z_order, osm_feature_polys.osm_id
+          SELECT * FROM (
+            SELECT DISTINCT ON (osm_landusages.osm_id)
+              osm_landusages.geometry, osm_landusages.name, osm_landusages.area,
+              osm_landusages.type IN ('forest', 'wood', 'scrub', 'heath', 'grassland', 'scree', 'meadow', 'fell', 'wetland') AS natural,
+              z_order,
+              osm_landusages.osm_id AS osm_id
+            FROM
+              osm_landusages
+            LEFT JOIN
+              z_order_landuse USING (type)
+            LEFT JOIN
+              osm_feature_polys USING (osm_id)
+            LEFT JOIN
+              -- NOTE filtering some POIs (hacky because it affects also lower zooms)
+              osm_sports ON osm_landusages.osm_id = osm_sports.osm_id AND osm_sports.type IN ('soccer', 'tennis', 'basketball')
+            WHERE
+              osm_feature_polys.osm_id IS NULL AND osm_sports.osm_id IS NULL AND osm_landusages.geometry && !bbox!
+            ORDER BY
+              osm_landusages.osm_id, osm_landusages.type IN ('forest', 'wood', 'scrub', 'heath', 'grassland', 'scree', 'meadow', 'fell', 'wetland') DESC
+          ) foo ORDER BY z_order, osm_id
         "
       />
     </>
