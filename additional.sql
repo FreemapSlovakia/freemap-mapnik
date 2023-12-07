db3a8e50409532590fa0220c87e0a99af175e6fc
Martin Ždila <m.zdila@gmail.com>
2023-12-07 07:29:32 +0100

    fixed sql fmt

--- additional.sql
+++ additional.sql
@@ -5,11 +5,9 @@ CREATE EXTENSION hstore;
 -- see https://wiki.postgresql.org/wiki/First/last_(aggregate)
 -- Create a function that always returns the first non-NULL item
 CREATE
-OR REPLACE FUNCTION public.first_agg (anyelement, anyelement) RETURNS anyelement LANGUAGE SQL IMMUTABLE STRICT AS $ $
-SELECT
-        $ 1;
-
-$ $;
+OR REPLACE FUNCTION public.first_agg (anyelement, anyelement) RETURNS anyelement LANGUAGE SQL IMMUTABLE STRICT AS $$
+SELECT $1;
+$$;
 
 -- And then wrap an aggregate around it
 CREATE AGGREGATE public.FIRST (
@@ -20,11 +18,9 @@ CREATE AGGREGATE public.FIRST (
 
 -- Create a function that always returns the last non-NULL item
 CREATE
-OR REPLACE FUNCTION public.last_agg (anyelement, anyelement) RETURNS anyelement LANGUAGE SQL IMMUTABLE STRICT AS $ $
-SELECT
-        $ 2;
-
-$ $;
+OR REPLACE FUNCTION public.last_agg (anyelement, anyelement) RETURNS anyelement LANGUAGE SQL IMMUTABLE STRICT AS $$
+SELECT $2;
+$$;
 
 -- And then wrap an aggregate around it
 CREATE AGGREGATE public.LAST (
